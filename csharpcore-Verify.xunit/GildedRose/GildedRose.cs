ab74d7992982a12d03049e972286d80d77ababd9
Katrien D'Hoker <katrien.dhoker@mstack.nl>
2024-05-24 15:46:43 +0200

    Phase 3: clean up

--- csharpcore-Verify.xunit/GildedRose/GildedRose.cs
+++ csharpcore-Verify.xunit/GildedRose/GildedRose.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace GildedRoseKata
 {
@@ -14,76 +15,120 @@ namespace GildedRoseKata
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != ProductNames.Brie && Items[i].Name != ProductNames.BackstagePass)
+                UpdateQuality(Items[i]);
+
+                UpdateSellInDays(Items[i]);
+
+                if (Items[i].SellInDays < SellingDays.DaySoldDue)
                 {
-                    if (Items[i].Quality > Quality.MinimalQuality)
+                    UpdateQualityForSoldDue(Items[i]);
+
+                }
+            }
+        }
+
+        private static void UpdateQuality(Item item)
+        {
+            if (IsQualityDecreaseAllowedFor(item.Name))
+            {
+                UpdateQualityForDecrease(item);
+            }
+
+            if (IsQualityIncreaseAllowedFor(item.Name))
+            {
+                UpdateQualityForIncrease(item);
+            }
+        }
+        
+        private void UpdateSellInDays(Item item)
+        {
+            if (IsSellingDaysDecreaseAllowedFor(item.Name))
+            {
+                item.SellInDays = Decrease(item.SellInDays);
+            }
+        }
+
+        private void UpdateQualityForSoldDue(Item item)
+        {
+            if (IsQualityResetAllowedForSoldDue(item))
+            {
+                item.Quality = Quality.MinimalQuality;
+            }
+
+            if (IsQualityDecreaseAllowedForSoldDue(item))
+            { 
+                item.Quality = Decrease(item.Quality); 
+            }
+
+            if (IsQualityIncreaseAllowedForSoldDue(item))
+            {
+                item.Quality = Increase(item.Quality);
+            }
+        }
+
+        private static bool IsQualityDecreaseAllowedForSoldDue(Item item)
+            =>  item.Name != ProductNames.Brie && 
+                item.Name != ProductNames.BackstagePass &&
+                item.Name != ProductNames.Sulfuras &&
+                item.Quality > Quality.MinimalQuality;
+
+        private static bool IsQualityResetAllowedForSoldDue(Item item)
+            => item.Name == ProductNames.BackstagePass;
+
+        private static bool IsSellingDaysDecreaseAllowedFor(string name)
+            => name != ProductNames.Sulfuras;
+
+        private static bool IsQualityDecreaseAllowedFor(string name)
+            => name != ProductNames.Brie && name != ProductNames.BackstagePass;
+
+        private static bool IsQualityIncreaseAllowedFor(string name)
+            => name == ProductNames.Brie || name == ProductNames.BackstagePass;
+
+        private static void UpdateQualityForIncrease(Item item)
+        {
+            if (item.Quality < Quality.MidQuality)
+            {
+                item.Quality = Increase(item.Quality);
+
+                if (item.Name == ProductNames.BackstagePass)
+                {
+                    if (item.SellInDays < SellingDays.DayEleven)
                     {
-                        if (Items[i].Name != ProductNames.Sulfuras)
+                        if (item.Quality < Quality.MidQuality)
                         {
-                            Items[i].Quality = Items[i].Quality - 1;
+                            item.Quality = Increase(item.Quality);
                         }
                     }
-                }
-                else
-                {
-                    if (Items[i].Quality < Quality.MidQuality)
+
+                    if (item.SellInDays < SellingDays.DaySix)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == ProductNames.BackstagePass)
+                        if (item.Quality < Quality.MidQuality)
                         {
-                            if (Items[i].SellInDays < SellingDays.DayEleven)
-                            {
-                                if (Items[i].Quality < Quality.MidQuality)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellInDays < SellingDays.DaySix)
-                            {
-                                if (Items[i].Quality < Quality.MidQuality)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != ProductNames.Sulfuras)
-                {
-                    Items[i].SellInDays = Items[i].SellInDays - 1;
-                }
-
-                if (Items[i].SellInDays < 0)
-                {
-                    if (Items[i].Name != ProductNames.Brie)
-                    {
-                        if (Items[i].Name != ProductNames.BackstagePass)
-                        {
-                            if (Items[i].Quality > Quality.MinimalQuality)
-                            {
-                                if (Items[i].Name != ProductNames.Sulfuras)
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < Quality.MidQuality)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
+                            item.Quality = Increase(item.Quality);
                         }
                     }
                 }
             }
         }
+
+        private static void UpdateQualityForDecrease(Item item)
+        {
+            if (item.Quality > Quality.MinimalQuality)
+            {
+                if (item.Name != ProductNames.Sulfuras)
+                {
+                    item.Quality = Decrease(item.Quality);
+                }
+            }
+        }
+
+        private static int Increase(int number)
+            => ++number;
+
+        private static int Decrease(int number)
+            => --number;
+
+        private static bool IsQualityIncreaseAllowedForSoldDue(Item item)
+            => item.Name == ProductNames.Brie && item.Quality < Quality.MidQuality;
     }
 }
--- csharpcore-Verify.xunit/GildedRose/SellingDays.cs
+++ csharpcore-Verify.xunit/GildedRose/SellingDays.cs
@@ -2,6 +2,7 @@
 {
     internal class SellingDays
     {
+        public const int DaySoldDue = 0;
         public const int DayEleven = 11;
         public const int DaySix = 6;
     }
